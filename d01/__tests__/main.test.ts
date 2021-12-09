0ed5ed571dc1fdf73d85f185b7574858b567c1d7
Elliot Bouvier <elliotbouvier@protonmail.com>
2021-12-09 20:32:27 +0100

    feat: part2

--- d01/__tests__/main.test.ts
+++ d01/__tests__/main.test.ts
@@ -1,21 +1,22 @@
-import { part1 } from '../main'
+import { part1, part2 } from '../main'
 
 describe('d01', () => {
-    it('should pass', () => {
-        const input = `
-        199
-        200
-        208
-        210
-        200
-        207
-        240
-        269
-        260
-        263
-        `
-        const r = part1(input)
-        expect(r).toBeTruthy()
-        expect(r).toEqual(7)
+    const input = `199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263`
+        .split('\n')
+        .map(Number)
+    it('part1 should pass', () => {
+        expect(part1(input)).toEqual(7)
+    })
+    it('part2 should pass', () => {
+        expect(part2(input)).toEqual(5)
     })
 })
--- d01/main.ts
+++ d01/main.ts
@@ -1,17 +1,22 @@
 import { readFileSync } from 'fs'
 
 const rf = (input: string) => readFileSync(input, 'utf8')
-const data = rf('d01/input')
-/**
- * part 1
- * @param input
- * @returns
- */
-export const part1 = (input: string) => {
-    const depths = input.split('\n').map((e) => Number(e))
-    return depths.reduce((acc, curr, i) => {
-        return depths[i - 1] && depths[i - 1] < curr ? (acc += 1) : acc
+const data = rf('d01/input').split('\n').map(Number)
+
+export const part1 = (input: number[]) => {
+    return input.reduce((acc, curr, i, arr) => {
+        return arr[i - 1] && arr[i - 1] < curr ? (acc += 1) : acc
     }, 0)
+}
+export const part2 = (input: number[]) => {
+    const measurementWindows = input
+        .map((_, i) => input.slice(i, i + 3).reduce((a, b) => a + b), 0)
+        .reduce((acc, curr, i, arr) => {
+            return arr[i - 1] && arr[i - 1] < curr ? (acc += 1) : acc
+        }, 0)
+
+    return measurementWindows
 }
 
 console.log(part1(data))
+console.log(part2(data))
